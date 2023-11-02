690616dd4f243b16e8faa80b0064f019aa6ae4a3
unknown <superpegorino@gmail.com>
2023-11-01 23:23:06 -0500

    tags models and view

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -9,15 +9,17 @@ from django.contrib.auth import get_user_model
 from core import models
 
 
+def create_user(email="user@example.com", password="testpass123"):
+    return get_user_model().objects.create_user(email, password)
+
+
 class ModelTests(TestCase):
     """Test models."""
 
     def test_create_user_with_email_successful(self):
         email = "test@example.com"
         password = "testpass123"
-        user = get_user_model().objects.create_user(
-            email=email, password=password
-            )
+        user = get_user_model().objects.create_user(email=email, password=password)
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
@@ -43,24 +45,29 @@ class ModelTests(TestCase):
     def test_create_superuser(self):
         """Test creating a superuser"""
 
-        user = get_user_model().objects.create_superuser(
-            "test@example.com", "test123"
-            )
+        user = get_user_model().objects.create_superuser("test@example.com", "test123")
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
 
     def test_create_recipe(self):
         """Test creating a recipe is successful."""
         user = get_user_model().objects.create_user(
-            'test@example.com',
-            'testpass123',
+            "test@example.com",
+            "testpass123",
         )
         recipe = models.Recipe.objects.create(
             user=user,
-            title='Sample recipe name',
+            title="Sample recipe name",
             time_minutes=5,
-            price=Decimal('5.50'),
-            description='Sample recipe description',
+            price=Decimal("5.50"),
+            description="Sample recipe description",
         )
 
         self.assertEqual(str(recipe), recipe.title)
+
+    def test_create_tag(self):
+        """Test creating a tag is successful"""
+        user = create_user()
+        tag = models.Tag.objects.create(user=user, name="Tag1")
+
+        self.assertEqual(str(tag), tag.name)
